fb1d40de17f2a08439ec70e17f4e4c1b25886aad
DatOneBrainCell <crjasperdaniel@gmail.com>
2026-07-01 11:39:43 +0530

    Create ReverseAString.py

---
+++ Assignments/FnsModsPackages/ReverseAString.py
@@ -0,0 +1,10 @@
+def ReverseOfAStr(s):
+    s = s[::-1]
+    return s
+    
+s = "Hannibal Barca was a Carthegenian General"
+print(ReverseOfAStr(s))
+
+'''O/P:
+lareneG nainegehtraC a saw acraB labinnaH
+'''
