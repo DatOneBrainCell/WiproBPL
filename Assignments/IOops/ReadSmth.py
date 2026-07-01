8067186348014677ae91c448c2a6429870bf7815
DatOneBrainCell <crjasperdaniel@gmail.com>
2026-07-01 12:04:27 +0530

    Create ReadSmth.py

---
+++ Assignments/IOops/ReadSmth.py
@@ -0,0 +1,9 @@
+with open("C:\\Users\\Admin\\Desktop\\23CS141\\txt\\SomethingAboutHistory.txt", "r") as tx:
+    content = tx.read()
+    print(content)
+
+'''O\P:
+Napoleon Bonaparte was a French general
+Hannibal Barca was a Carthegenian general
+Alexander the Great was a Roman general
+'''
