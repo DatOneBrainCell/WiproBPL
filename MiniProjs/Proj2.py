fa58050490f8b293c19e4ecad7c2edd596076781
DatOneBrainCell <crjasperdaniel@gmail.com>
2026-07-01 23:38:37 +0530

    Create Proj2.py

---
+++ MiniProjs/Proj2.py
@@ -0,0 +1,14 @@
+def GetRunnerUpScore(scores):
+    unique = set(scores)
+    sortedLi = sorted(list(unique))
+    
+    if len(sortedLi) >= 2:
+        return sortedLi[-2]
+    else:
+        return "Cant determine runner up"
+
+li = [2, 3, 6, 6, 5]
+runnerUp = GetRunnerUpScore(li)
+
+print("Sample Input: ", li)
+print("Sample Output: ", runnerUp)
