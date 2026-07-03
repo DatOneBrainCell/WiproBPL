8634456986c2ec1ab057f33ba12bf7843a2ca7b9
DatOneBrainCell <crjasperdaniel@gmail.com>
2026-07-03 11:35:58 +0530

    Create Welcome.py

---
+++ Assignments/CmdLineArgs/Welcome.py
@@ -0,0 +1,13 @@
+import sys
+
+fileName = sys.argv[0]
+msg = " ".join(sys.argv[1:])
+
+print(fileName)
+print(msg)
+
+'''O/P:
+$ python Welcome.py Hello and welcome to Python!
+assignment2.py
+Hello and welcome to Python!
+'''
