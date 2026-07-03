57304056c7525e231d8a9d5ebf95e369d3398a6e
DatOneBrainCell <crjasperdaniel@gmail.com>
2026-07-03 12:40:56 +0530

    Create Server.py

---
+++ MiniProjs/Fundamentals/Server.py
@@ -0,0 +1,13 @@
+charges = 0.51
+
+print("Per day:", charges * 24)
+print("Per week:", charges * 168)
+print("Per month:", charges * 730)
+print("Days operable with $918:", 918/charges)    
+
+'''O/P:
+Per day: 12.24
+Per week: 85.68
+Per month: 372.3
+Days operable with $918: 1800.0
+'''
