d112314210ec104139047e52f642b1b42665369d
DatOneBrainCell <crjasperdaniel@gmail.com>
2026-07-07 10:23:33 +0530

    Delete Assignments/DataStructs/Strings directory

--- Assignments/DataStructs/Strings/NumsAndPows.py
+++
@@ -1,7 +0,0 @@
-dic = dict()
-
-for i in range(1, 16):
-    dic[i] = i**2
-
-for keys, vals in dic.items():
-    print(keys, ": ", vals)
--- Assignments/DataStructs/Strings/SeeIfKeyExists.py
+++
@@ -1,9 +0,0 @@
-dic = {1: 10, 2: 20, 3: 30}
-print(dic)
-
-ip = int(input("Enter a key: "))
-
-if ip in dic.keys():
-    print("It exists")
-else:
-    print("It doesnt exist")
