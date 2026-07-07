a9634117338ee67e4b0e8ecfc363fee223601b21
DatOneBrainCell <crjasperdaniel@gmail.com>
2026-07-07 10:33:29 +0530

    Create Intersection.py

---
+++ Assignments/DataStructs/Sets/Intersection.py
@@ -0,0 +1,14 @@
+s1 = {"Vlad", "Hannibal", "Napoleon"}
+print(s1)
+s2 = {"Kutty Mathan", "Vlad", "Napoleon"}
+print(s2)
+print()
+s3 = s1.intersection(s2)
+print("Intersecrion: ", s3)
+
+'''O/P:
+{'Vlad', 'Napoleon', 'Hannibal'}
+{'Vlad', 'Napoleon', 'Kutty Mathan'}
+
+Intersecrion:  {'Vlad', 'Napoleon'}
+'''
